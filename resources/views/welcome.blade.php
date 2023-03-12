4ab08598e938340275af64d09a938d8864d779ee
AdrianVyne <imadrian1234@gmail.com>
2023-03-12 14:38:15 +0800

    .

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,6 +3,8 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
+
         @include("layouts/head")
         <title>Laravel 10 Vite with React</title>
         @viteReactRefresh
@@ -11,6 +13,6 @@
     <body>
     @include("layouts/navbar-user")
      <div id="app"></div>
-     <p>in essence this website is runs through php with the laravel framework with vite and react.js </p>
+     <p>in essence this website runs through php with the laravel framework with vite and react.js </p>
     </body>
 </html>
