de5f946888463842799600338f25c579d2cb9a6a
AdrianVyne <imadrian1234@gmail.com>
2023-03-12 10:54:13 +0800

    testing y home page can't be found

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,13 +1,17 @@
 <!DOCTYPE html>
 <html lang="en">
 
+
 <head>
     @include("layouts/head")
-    <title>Document</title>
-</head>
 
+    <title>Document</title>
+    @viteReactRefresh
+        @vite('resources/js/app.jsx')
+</head>
+   
 <body>
-@include("layouts/navbar-user")
+ @include("layouts/navbar-user")
 <h1>About Page</h1>
 <p>this page is called through route that would direct to a blade</p>
 </body>
